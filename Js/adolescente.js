2c37cb66fc60234b1156ec3b9fa8dcc7d1a205a6
GlaucoTolotto <glaucotolotto@gmail.com>
2021-04-13 21:13:28 -0300

    Ajuste das respostas das fases, ajuste do alert quando mostra as respostas.

--- Js/adolescente.js
+++ Js/adolescente.js
@@ -12,7 +12,7 @@ function adolescente(){
         <div class="caixaDeConteudo">
             <div class="textoDoPersogem">
             <h1>Adolescente baixando filme pirata</h1>
-                <p>Seu persogem estavá calmamente baixando filmes para poder ver com sua Web namorada, mais tarde naquele dia, a garota mais linda da internet (segundo fotos, pois ela sempre se recusa a ligar a camera, e sempre tem uma desculpa para não se encontrar com o nosso herói)</p>
+                <p>Seu persogem estáva calmamente baixando filmes para poder ver com sua Web namorada a garota mais linda da internet (segundo fotos, pois ela sempre se recusa a ligar a camera, e sempre tem uma desculpa para não se encontrar com o nosso herói)</p>
                 <p>Mas derrepente se viu dentro de um jogo estranho, que jamais virá em sua vida toda (O que não é muito tempo, pois estamos falando de um adolescente). Agora ele faz parte do jogo como um persogem.</p>
             </div>
             <img src="./imagens/Adolescente.jpg">
